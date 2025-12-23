cf54c940f45531c4b539cf1e5870fa70f7caaba4
VickDaPro <vickbishnoi@gmail.com>
2025-12-24 00:50:27 +0530

    Refactor user component to emit selected user ID and update app component to pass user ID

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,11 @@
 import {
   Component,
   computed,
+  EventEmitter,
   Input,
   input,
+  output,
+  Output,
   // computed,
   //  signal
 } from '@angular/core';
@@ -18,17 +21,23 @@ import {
   styleUrl: './user.component.css',
 })
 export class UserComponent {
+  @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
   // avatar = input.required<string>();
   // name = input.required<string>();
+
+  @Output() select = new EventEmitter();
+  // select = output<string>();
 
   // imagePath = computed(() => 'assets/users/' + this.avatar());
   get imagePath() {
     return 'assets/users/' + this.avatar;
   }
 
-  onSelectUser() {}
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
 
   // selectedUser = signal(DUMMY_USERS[randomIndex]);
 
